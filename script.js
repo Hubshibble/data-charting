dfd0ec8f3cee52c47dc24292642ab891499bebce
mohammad-alfatih <alfaitha@gmail.com>
2020-01-31 08:47:00 -0500

    add method for building chart

--- script.js
+++ script.js
@@ -3,6 +3,20 @@
     //     return ""
     // }
 
+    function buildChart() {
+        c3.generate({
+            bindto: '#chart',
+            data: {
+                columns: [
+                    ['data1', 30, 200, 100, 400, 150, 250],
+                    ['data2', 50, 20, 10, 40, 15, 25]
+                ]
+            }
+        });
+    }
+
+    buildChart();
+
     function loadChart(cols, restraints) {
         var cols = '*';
         var restraints = '';
